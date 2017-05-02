c9b1d443a0be8785fb2c3eeb63ef5a085a61e891
KotoriCANOE <sanma.kote@gmail.com>
2017-05-02 05:31:03 +0100

    Update

--- TaxiDrive/js/main.js
+++ TaxiDrive/js/main.js
@@ -12,13 +12,10 @@ class TaxiDrive
 
         this.checkDuration = 200; // duration for checking data re-loading (microsecond)
 
-        var tsPerSec = 360; // elapsed timestamp per second
+        let tsPerSec = 1200; // elapsed timestamp per second
         this.simTick = 40; // simulation tick (microsecond)
-        var tickPerSec = 1000 / this.simTick;
+        let tickPerSec = 1000 / this.simTick;
         this.tickTime = tsPerSec / tickPerSec;
-        this.longiRange = [116, 116.8];
-        var latiRangeHalf = (this.longiRange[1] - this.longiRange[0]) / width * height * 0.5;
-        this.latiRange = [39.9 - latiRangeHalf, 39.9 + latiRangeHalf];
 
         // options
         this.width = width;
@@ -35,12 +32,29 @@ class TaxiDrive
         this.width -= this.margin.left + this.margin.right;
         this.height -= this.margin.top + this.margin.bottom;
 
-        this.xScale = d3.scaleLinear()
+        this.m1 = new Object();
+        this.m2 = new Object();
+        this.mSelect = 2;
+
+        // scaling
+        this.longiRange = [116, 116.8];
+        let latiRangeHalf = (this.longiRange[1] - this.longiRange[0])
+            / this.width * this.height * 0.5;
+        this.latiRange = [39.9 - latiRangeHalf, 39.9 + latiRangeHalf];
+
+        this.m1.xScale = d3.scaleLinear()
             .domain(this.longiRange)
-            .range([0, width]);
-        this.yScale = d3.scaleLinear()
+            .rangeRound([0, width]);
+        this.m1.yScale = d3.scaleLinear()
             .domain(this.latiRange)
-            .range([height, 0]);
+            .rangeRound([height, 0]);
+
+        this.m2.xScale = d3.scaleLinear()
+            .domain(this.longiRange)
+            .rangeRound([0, width]);
+        this.m2.yScale = d3.scaleLinear()
+            .domain(this.latiRange)
+            .rangeRound([height, 0]);
 
         // color
         function opacity2color(opacity)
@@ -48,60 +62,61 @@ class TaxiDrive
             return Math.floor(0.5 / (1 - opacity));
         }
 
-        var opacityPerSec = 0.65;
-        var opacityPerTick = Math.pow(opacityPerSec, 1 / tickPerSec);
-        var backColor = 0;//opacity2color(opacityPerTick)
-        this.m1 = {
-            refreshColor: 'rgba(0,0,0,' + (1 - opacityPerTick) + ')',
-            backColor: 'rgb(' + backColor + ',' + backColor + ',' + backColor + ')',
-            dotColor: 'rgb(255, 255, 128)',
+        {
+            let opacityPerSec = 0.5;
+            let opacityPerTick = Math.pow(opacityPerSec, 1 / tickPerSec);
+            let backColor = opacity2color(opacityPerTick);
+            this.m1.refreshColor = 'rgba(0,0,0,' + (1 - opacityPerTick) + ')';
+            this.m1.backColor = 'rgb(' + backColor + ',' + backColor + ',' + backColor + ')';
+            //this.m1.dotColor = 'rgb(255, 255, 128)';
+        }
+
+        {
+            this.m2.backColor = 'rgb(0,0,0)';
         }
     }
 
     // Main methods
     initialize(data)
     {
+        // initialize data
         data.nodes = new Array();
         data.index = 0;
         data.ts = 0;
         data.tsupper = 0;
+
+        // initialize cumulate map
+        data.cumulateMap = new Array(this.width * this.height);
+        data.cumulateMap.fill(0);
+        data.cumulateImage = this.context.createImageData(this.width, this.height);
+        canvasImageInit(data.cumulateImage);
     }
 
     load(data)
     {
-        var that = this;
-        var csvPrefix = '_data/ts-split/dataset.timestamp.';
-        var csvPostfix = '.csv';
-        var csvNo = 1;
-        var csvNoUpper = 8;
+        let that = this;
+        let csvPrefix = '_data/ts-split/dataset.timestamp.';
+        let csvPostfix = '.csv';
+        let csvNoUpper = 8;
+        data.csvNo = 1;
 
         // load CSV with a callback on loading successful
         function loadCSV(data, callback)
         {
-            if(csvNo < csvNoUpper)
+            if(data.csvNo < csvNoUpper)
             {
-                var csv = csvPrefix + csvNo + csvPostfix;
-                console.log('Loading...: ' + csv);
-                d3.csv(csv)
-                    /*.row(function(d)
-                    {
-                        return {
-                            taxiID: +d.TaxiID,
-                            dateTime: d.DateTime,
-                            longitude: +d.Longitude,
-                            latitude: +d.Latitude,
-                            timestamp: +d.Timestamp
-                        }
-                    })*/
-                    .get(function(d)
-                    {
-                        data.nodes = d;
-                        data.index = -1;
-                        data.ts = +data.nodes[0].Timestamp;
-                        data.tsupper = data.ts;
-                        ++csvNo;
-                        callback();
-                    });
+                let csv = csvPrefix + data.csvNo + csvPostfix;
+                console.log('Loading ' + csv + ' ...');
+                d3.csv(csv).get(function(d)
+                {
+                    data.nodes = d;
+                    data.index = -1;
+                    data.ts = +data.nodes[0].Timestamp;
+                    data.tsupper = data.ts;
+                    ++data.csvNo;
+                    console.log('Successfully loaded!');
+                    callback();
+                });
             }
         }
 
@@ -129,50 +144,94 @@ class TaxiDrive
 
     draw(data)
     {
-        var that = this;
-        var fpsCounter = new RateCounter(50, 1000);
+        let that = this;
+        let dateTime = '';
+        let mSelect = that.mSelect;
+        let m = that['m' + mSelect];
 
-        this.context.fillStyle = that.m1.backColor;
+        this.context.fillStyle = m.backColor;
         this.context.fillRect(-that.margin.left, -that.margin.top,
             that.canvas.attr('width'), that.canvas.attr('height'));
 
         // draw animation up to the limitation of display refreshing
         d3.interval(function(elapsed)
         {
-            // assign from data
-            var nodes = data.nodes;
-            var index = data.index;
-            var ts = data.ts;
-            var tsupper = data.tsupper + that.tickTime;
-            var length = nodes.length;
+            let m1 = that.m1;
+            let m2 = that.m2;
+            let mSelect = that.mSelect;
+            let m = that['m' + mSelect];
+            let width = that.width;
+            let height = that.height;
 
-            // skip if current data's drawing is finished
-            if(index >= length) return;
+            // assign from data
+            let nodes = data.nodes;
+            let index = data.index;
+            let ts = data.ts;
+            let tsupper = data.tsupper + that.tickTime;
+            let length = nodes.length;
+            let cumulateMap = data.cumulateMap;
+            let cumulateImage = data.cumulateImage;
+            let cumulateIData = data.cumulateImage.data;
+
+            // skip if current data is not available or data's drawing is finished
+            if(length <= 0 || index >= length) return;
 
             // Canvas refreshing
-            var context = that.context;
+            let context = that.context;
 
-            context.fillStyle = that.m1.refreshColor;
-            context.fillRect(-that.margin.left, -that.margin.top,
-                that.canvas.attr('width'), that.canvas.attr('height'));
+            if(mSelect === 1)
+            {
+                context.fillStyle = that.m1.refreshColor;
+                context.fillRect(-that.margin.left, -that.margin.top,
+                    that.canvas.attr('width'), that.canvas.attr('height'));
+            }
 
             // Canvas drawing
             while(++index < length)
             {
-                var d = nodes[index];
+                let d = nodes[index];
                 ts = +d.Timestamp;
                 if(ts >= tsupper) break;
+                let longitude = +d.Longitude;
+                let latitude = +d.Latitude;
 
-                var x = that.xScale(+d.Longitude);
-                var y = that.yScale(+d.Latitude);
+                // draw to instant map
+                if(mSelect === 1)
+                {
+                    let x = m1.xScale(longitude);
+                    let y = m1.yScale(latitude);
+                    let fill = d3.interpolateRainbow((+d.TaxiID - 1) / 10356);
+                    context.fillStyle = fill;
+                    context.fillRect(x, y, 1, 1);
+                }
 
-                context.fillStyle = that.m1.dotColor;
-                context.fillRect(x, y, 1, 1);
+                // accumulate to cumulate map
+                {
+                    let x = m2.xScale(longitude);
+                    let y = m2.yScale(latitude);
+                    if(x >= 0 && x < width && y >= 0 && y < height)
+                    { // filter out of boundary data
+                        let index = width * y + x;
+                        let value = cumulateMap[index] += 1;
+                        let color = d3.color(d3.interpolateInferno(
+                            Math.min(1, Math.log2(value + 1) * 0.125)));
+                        let index2 = index * 4;
+                        cumulateIData[index2] = color.r;
+                        cumulateIData[index2 + 1] = color.g;
+                        cumulateIData[index2 + 2] = color.b;
+                    }
+                }
             }
 
-            // draw FPS
-            fpsCounter.elapsed(elapsed);
-            fpsCounter.drawCanvas(context, true, 10, 20, 15, 'FPS: ');
+            // draw to cumulate map
+            if(mSelect === 2)
+            {
+                context.putImageData(cumulateImage, 0, 0);
+            }
+
+            // draw DateTime
+            if(index < length && index > 1) dateTime = nodes[index - 1].DateTime;
+            canvasDrawText(context, dateTime, m.backColor, 10, 20, 15);
 
             // assign to data
             data.index = index;
@@ -183,7 +242,7 @@ class TaxiDrive
 
     Run()
     {
-        var data = new Object();
+        let data = new Object();
         this.initialize(data);
         this.load(data);
         this.draw(data);
--- js/Helper.js
+++ js/Helper.js
@@ -12,7 +12,7 @@ function randomRangeInt(lower, upper)
 // break the original order
 function arrayRemove(array, index)
 {
-    var last = array.pop();
+    let last = array.pop();
     if(index < array.length) array[index] = last;
 }
 
@@ -33,7 +33,7 @@ class RateCounter
         this.rateScale = this.sampleSize * timeScale;
         this.durations = new Array(this.sampleSize);
 
-        for(var i = 0; i < this.sampleSize; ++i)
+        for(let i = 0; i < this.sampleSize; ++i)
         {
             this.durations[i] = 0;
         }
@@ -45,7 +45,7 @@ class RateCounter
 
     elapsed(elapsed)
     {
-        var duration = elapsed - this.lastElapsed;
+        let duration = elapsed - this.lastElapsed;
         this.lastElapsed = elapsed;
         this.sum += duration - this.durations[this.index];
         this.durations[this.index] = duration;
@@ -60,16 +60,80 @@ class RateCounter
     drawCanvas(context, clear = false, x = 10, y = 20,
         size = 15, prefix = 'Rate: ', fill = 'white')
     {
-        drawRate(this.rate(), context, clear, x, y, size, prefix, fill);
+        canvasDrawRate(context, this.rate(), clear, x, y, size, fill, prefix);
     }
 }
 
-function drawRate(rate, context, clear = false, x = 10, y = 20,
-    size = 15, prefix = 'Rate: ', fill = 'white')
+function canvasDrawText(context, text, clear = false, x = 10, y = 20,
+    size = 15, fill = 'white')
 {
-    var text = prefix + rate;
-    if(clear) context.clearRect(x, y - size, text.length * size, size);
+    if(clear)
+    {
+        if(clear === true) context.fillStyle = 'rgb(0,0,0)';
+        else context.fillStyle = clear;
+        context.fillRect(x, y - size, text.length * size, size);
+    }
     context.font = size + 'px Consolas';
     context.fillStyle = fill;
     context.fillText(text, x, y);
+}
+
+function canvasDrawRate(context, rate, clear = false, x = 10, y = 20,
+    size = 15, prefix = 'Rate: ', fill = 'white')
+{
+    canvasDrawText(context, prefix + rate, clear, x, y, size, fill);
+}
+
+function canvasImageInit(image)
+{
+    let iData = image.data;
+    let length = image.width * image.height * 4;
+    for(let index = 0; index < length; index += 4)
+    {
+        iData[index] = 0;
+        iData[index + 1] = 0;
+        iData[index + 2] = 0;
+        iData[index + 3] = 255;
+    }
+}
+
+function canvasImageFill(image, r, g, b, a = 255)
+{
+    let data = image.data;
+    let upper = image.width * image.height * 4;
+    for(let index = 0; index < upper; index += 4)
+    {
+        data[index] = r;
+        data[index + 1] = g;
+        data[index + 2] = b;
+        data[index + 3] = a;
+    }
+}
+
+function canvasImagePixel(image, x, y, r, g, b, a = 255)
+{
+    let index = (x + y * image.width) * 4;
+    let data = image.data;
+    data[index] = r;
+    data[index + 1] = g;
+    data[index + 2] = b;
+    data[index + 3] = a;
+}
+
+function canvasDataPixel(data, width, x, y, r, g, b, a = 255)
+{
+    let index = (x + y * width) * 4;
+    data[index] = r;
+    data[index + 1] = g;
+    data[index + 2] = b;
+    data[index + 3] = a;
+}
+
+function canvasDataValue(data, width, x, y, v, a = 255)
+{
+    let index = (x + y * width) * 4;
+    data[index] = v;
+    data[index + 1] = v;
+    data[index + 2] = v;
+    data[index + 3] = a;
 }
